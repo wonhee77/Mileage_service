57f289bcb3c0ac21aca2b9df4bed7b3c7e02db78
wonhee77 <leehi402@naver.com>
2021-09-13 12:43:27 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/triple/wonhee/mileageservice/MileageServiceApplication.java
@@ -0,0 +1,13 @@
+package triple.wonhee.mileageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/triple/wonhee/mileageservice/MileageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package triple.wonhee.mileageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
