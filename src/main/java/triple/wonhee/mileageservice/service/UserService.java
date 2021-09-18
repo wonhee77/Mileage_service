15a47fe69a184300f12f9c75c7bf9e3ce7e1de75
wonhee77 <leehi402@naver.com>
2021-09-18 16:46:29 +0900

    feat: user에 대한 point 조회 service 구현

---
+++ src/main/java/triple/wonhee/mileageservice/service/UserService.java
@@ -0,0 +1,24 @@
+package triple.wonhee.mileageservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import triple.wonhee.mileageservice.domain.User;
+import triple.wonhee.mileageservice.dto.UserPointResponseDto;
+import triple.wonhee.mileageservice.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserPointResponseDto getUserPoint(String userId) {
+        User user = userRepository.findByUserId(userId).orElseThrow(
+            () -> new NullPointerException("해당 id의 유저가 없습니다.")
+        );
+        UserPointResponseDto userPointResponseDto = new UserPointResponseDto(user.getUserPoint());
+        return userPointResponseDto;
+    }
+}
