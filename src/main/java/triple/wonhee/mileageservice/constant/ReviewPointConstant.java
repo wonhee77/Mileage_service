5b3f47541c076e42924608f4381775bb88b2b7ed
wonhee77 <leehi402@naver.com>
2021-09-19 00:53:54 +0900

    feat: reviewPoint 점수에 대한 상수를 Class로 분리

---
+++ src/main/java/triple/wonhee/mileageservice/constant/ReviewPointConstant.java
@@ -0,0 +1,9 @@
+package triple.wonhee.mileageservice.constant;
+
+public class ReviewPointConstant {
+
+    public static final int CONTENT_POINT = 1;
+    public static final int PHOTO_POINT = 1;
+    public static final int FIRST_REVIEW_POINT = 1;
+
+}
