aa749cb09508b8d5d58c2ffb73ef6f3167aab844
wonhee77 <leehi402@naver.com>
2021-09-18 16:36:11 +0900

    feat: Place entity에 placeName 항목 추가, 생성자 생성

--- src/main/java/triple/wonhee/mileageservice/domain/Place.java
+++ src/main/java/triple/wonhee/mileageservice/domain/Place.java
@@ -2,12 +2,22 @@ package triple.wonhee.mileageservice.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "PLACE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Place {
 
     @Id
     String placeId;
+
+    String placeName;
+
+    @Builder
+    public Place(String placeId, String placeName) {
+        this.placeId = placeId;
+        this.placeName = placeName;
+    }
 }
