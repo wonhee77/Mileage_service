2ed527875276206e09ac5712f6f39ea91a109c28
wonhee77 <leehi402@naver.com>
2021-09-18 16:40:22 +0900

    feat: ReviewRequestDto -> EventRequestDto로 refactoring

--- src/main/java/triple/wonhee/mileageservice/dto/EventRequestDto.java
+++ src/main/java/triple/wonhee/mileageservice/dto/EventRequestDto.java
@@ -3,11 +3,18 @@ package triple.wonhee.mileageservice.dto;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import triple.wonhee.mileageservice.domain.ActionType;
+import triple.wonhee.mileageservice.domain.EventType;
 
 @Getter
-public class ReviewRequestDto {
+public class
+EventRequestDto {
+
+    EventType type;
+    ActionType action;
+    String reviewId;
     String content;
-    List attachedPhotoIds = new ArrayList();
+    List attachedPhotoIds = new ArrayList<>();
     String userId;
     String placeId;
 }
