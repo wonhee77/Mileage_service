a0f1ea3caf3f21fbef92bcccc5b7e41dd77c2f5c
wonhee77 <leehi402@naver.com>
2021-09-18 16:35:11 +0900

    feat: Review entity에 reviewPoint 항목 추가

--- src/main/java/triple/wonhee/mileageservice/domain/Review.java
+++ src/main/java/triple/wonhee/mileageservice/domain/Review.java
@@ -2,18 +2,15 @@ package triple.wonhee.mileageservice.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-//@Table(indexes = @Index(name = "i_place", columnList = "place"))
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
 
@@ -24,6 +21,8 @@ public class Review {
 
     boolean isFirstReview;
 
+    int reviewPoint;
+
     @ManyToOne(fetch = FetchType.LAZY)
     User user;
 
@@ -31,9 +30,11 @@ public class Review {
     Place place;
 
     @Builder
-    public Review(String reviewContent, boolean isFirstReview, User user, Place place) {
+    public Review(String reviewId, String reviewContent, boolean isFirstReview, int reviewPoint, User user, Place place) {
+        this.reviewId = reviewId;
         this.reviewContent = reviewContent;
         this.isFirstReview = isFirstReview;
+        this.reviewPoint = reviewPoint;
         this.user = user;
         this.place = place;
     }
