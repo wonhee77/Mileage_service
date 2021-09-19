e096509cb8a9d60d89d1c92fb7cd701b5707aa27
wonhee77 <leehi402@naver.com>
2021-09-19 17:56:58 +0900

    fix: ReviewRepository, findAllByPlace에서  Place 객체 의존성 제거 위해 JPQL로 함수 변경

--- src/main/java/triple/wonhee/mileageservice/repository/ReviewRepository.java
+++ src/main/java/triple/wonhee/mileageservice/repository/ReviewRepository.java
@@ -3,14 +3,15 @@ package triple.wonhee.mileageservice.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import triple.wonhee.mileageservice.domain.Place;
 import triple.wonhee.mileageservice.domain.Review;
 import triple.wonhee.mileageservice.domain.User;
 
 public interface ReviewRepository extends JpaRepository<Review, String> {
 
-    List<Review> findAllByPlace(Place place);
-
-    Optional<Review> findByUserAndPlace(User user, Place place);
+    @Query("select rv from Review as rv where rv.place.placeId = :placeId")
+    List<Review> findAllByPlaceId(@Param("placeId") String placeId);
 
 }
