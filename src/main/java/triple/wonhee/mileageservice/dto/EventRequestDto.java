12f7f16abbbb4b77e8e19461a595c65cd59b5979
wonhee77 <leehi402@naver.com>
2021-09-19 18:02:08 +0900

    feat: EventRequestDto 생성자 추가

--- src/main/java/triple/wonhee/mileageservice/dto/EventRequestDto.java
+++ src/main/java/triple/wonhee/mileageservice/dto/EventRequestDto.java
@@ -2,19 +2,31 @@ package triple.wonhee.mileageservice.dto;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import triple.wonhee.mileageservice.domain.ActionType;
 import triple.wonhee.mileageservice.domain.EventType;
 
 @Getter
-public class
-EventRequestDto {
+public class EventRequestDto {
 
     EventType type;
     ActionType action;
     String reviewId;
     String content;
-    List attachedPhotoIds = new ArrayList<>();
+    List<String> attachedPhotoIds = new ArrayList<>();
     String userId;
     String placeId;
+
+    @Builder
+    public EventRequestDto(EventType type, ActionType action, String reviewId, String content,
+        List<String> attachedPhotoIds, String userId, String placeId) {
+        this.type = type;
+        this.action = action;
+        this.reviewId = reviewId;
+        this.content = content;
+        this.attachedPhotoIds = attachedPhotoIds;
+        this.userId = userId;
+        this.placeId = placeId;
+    }
 }
